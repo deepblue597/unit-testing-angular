20c475ca79f033d70ddefb33041b24c6bb322728
deepblue597 <iasonas2001@hotmail.com>
2022-12-03 22:31:56 +0200

    5 steps done problem with api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,16 +8,38 @@ import { StringArraysComponent } from './string-arrays/string-arrays.component';
 import { SetupAndTeardownComponent } from './setup-and-teardown/setup-and-teardown.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+//import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FundamentalsComponent,
+    // FundamentalsComponent,
     StringArraysComponent,
     SetupAndTeardownComponent,
     HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent,
+    MessagesComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    // HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+    //   dataEncapsulation: false,
+    // }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/fundamentals/fundamentals.component.ts
+++ src/app/fundamentals/fundamentals.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { timer } from 'rxjs';
-import { MessageService } from '../message.service';
+//import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-fundamentals',
@@ -10,7 +10,7 @@ import { MessageService } from '../message.service';
 export class FundamentalsComponent implements OnInit {
   hideContent = true;
   severity = 'you have been warned';
-  constructor(private msgService: MessageService) {}
+  constructor() {} // private msgService: MessageService
 
   ngOnInit(): void {}
 
